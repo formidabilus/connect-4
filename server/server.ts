c5f5c85bbae9e707bfec99f91a4d4a68e1e6fb12
formidabilus <chiriac_razvan@ymail.com>
2023-11-24 21:41:57 +0200

    small progress, room id persist between pages now

--- server/server.ts
+++ server/server.ts
@@ -13,25 +13,32 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-  socket.on("player", (player, room) => {
+  socket.on("join-room", (room) => {
+    socket.join(room);
+    console.log("room from server join: ", room);
+  });
+  socket.on("connect", () => {
+    console.log("connected with socket id: ", socket.id);
+    socket.broadcast.emit("socketID", socket.id);
+  });
+  socket.on("send_player", (room, player) => {
     if (!room) {
       console.log("No room selected!");
     } else {
-      console.log("connected player: ", player);
       socket.to(room).emit("player", player);
+      console.log("connected player: ", player);
+      console.log("room from player server: ", room);
     }
   });
-  socket.on("colorTiles", (colorTiles, room) => {
+  socket.on("send_colorOfTiles", (room, colorOfTiles) => {
     if (!room) {
       console.log("No room selected!");
     } else {
-      console.log("colorTiles: ", colorTiles);
-      socket.broadcast.emit("colorTiles", colorTiles);
+      socket.to(room).emit("colorOfTiles", colorOfTiles);
+      console.log("colorOfTiles: ", colorOfTiles);
+      console.log("room from colorOfTiles server: ", room);
     }
   });
-  // socket.on('join-room', room) => {
-  //   socket.join(room)
-  // }
 });
 
 const port = 3001;

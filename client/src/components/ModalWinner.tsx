2c67ad01d803ff5fc4d897b0fd38638d0852dec6
formidabilus <chiriac_razvan@ymail.com>
2024-05-23 17:30:31 +0300

    added text color based on winner in ModalWinner component

--- client/src/components/ModalWinner.tsx
+++ client/src/components/ModalWinner.tsx
@@ -2,11 +2,22 @@ import Modal from "./Modal";
 
 export default function ModalWinner({ winner }: { winner: number }) {
   const result =
-    winner == 1 ? "red" : winner == 2 ? "yellow" : winner == 3 ? "draw" : null;
+    winner == 1 ? "Red" : winner == 2 ? "Yellow" : winner == 3 ? "Draw" : null;
   console.log("winner from winnerModal: ", winner);
   return (
     <Modal showModal={!winner}>
-      <h1>Winner: {result}</h1>
+      <h1
+        className={`${
+          winner == 2
+            ? "text-yellow-500"
+            : winner == 1
+            ? "text-red-500"
+            : "text-gray-500"
+        } text-7xl grid justify-center`}
+      >
+        {" "}
+        {winner < 3 ? `${result} Won!` : `It's a Draw!`}{" "}
+      </h1>
     </Modal>
   );
 }
